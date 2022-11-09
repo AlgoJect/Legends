a1c6f78a6edb8c03eec888c052b208e3af8152fd
Fahad Bin Hussain <fahadbinhussain@outlook.com>
2022-11-09 19:25:46 +0600

    Beautify major code

--- resources/login.js
+++ resources/login.js
@@ -3,9 +3,9 @@ const param = new URLSearchParams(window.location.search);
 const username = param.get('username');
 const pw = param.get('pw');
 
-if(username.toLowerCase() === 'shajibdipubandhan' && pw === 'friendshipforever'){
+if (username.toLowerCase() === 'shajibdipubandhan' && pw === 'friendshipforever') {
   message.innerHTML = 'Hurray for client-side validation!';
-} else if(!username || !pw){
+} else if (!username || !pw) {
   message.innerHTML = 'Add some client-side validation!';
 } else {
   message.innerHTML = 'Add some client-side validation!';
